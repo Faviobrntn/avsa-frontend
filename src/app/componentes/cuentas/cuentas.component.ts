eb3750e09a6eca608b5d67db8fe35d8fd9052821
favio <favio@syloper.com>
2020-07-14 15:42:30 -0300

    fix vulnerabilidades

--- src/app/componentes/cuentas/cuentas.component.ts
+++ src/app/componentes/cuentas/cuentas.component.ts
@@ -25,8 +25,8 @@ export class CuentasComponent implements AfterViewInit {
 	isLoadingResults = true;
 	isRateLimitReached = false;
 
-	@ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-	@ViewChild(MatSort, { static: false }) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
 	constructor(
 		private _httpClient: HttpClient,
